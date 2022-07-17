c6c1195ff1b7bd68e4f8d3086c67941aba89b2fd
willzfrank <willzudoh4@gmail.com>
2022-07-17 17:58:07 +0100

    working on graphql client

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const client = new ApolloClient({
+    uri: "https://flyby-gateway.herokuapp.com/",
+    cache: new InMemoryCache(),
+  });
+  return <ApolloProvider client={client}></ApolloProvider>;
 }
 
 export default App;
